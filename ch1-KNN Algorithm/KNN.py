a32035cd9614b07e4804df7e7c6722d580cdbb82
Adherersun <adherersun@163.com>
2018-03-16 21:48:49 +0800

    ch1-KNN Algorithm 2018.03.16 晚上9:48

---
+++ ch1-KNN Algorithm/KNN.py
@@ -0,0 +1,23 @@
+from numpy import *        #科学计算包
+import operator            #运算符模块
+
+# 创建数据集和标签
+def createDataSet():
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
+def classify0(inX, dataset, labels, k):
+    dataSetSize = dataset.shape[0]
+    diffMat = tile(inX, (dataSetSize, 1)) - dataset
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
